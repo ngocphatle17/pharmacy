3f37d72a3e6db1a96b1cbb720acb8492c30623ea
simon <pxl220003@gmail.com>
2026-04-22 14:03:19 -0500

    updates

--- js/layout.js
+++ js/layout.js
@@ -18,8 +18,6 @@ const headerHTML = `
         </span>
         <div class="dropdown">
           <a href="../pages/about.html">Our Story</a>
-          <a href="../pages/about.html#team">Our Team</a>
-          <a href="../pages/about.html#mission">Our Mission</a>
         </div>
       </div>
       <div class="nav-item">
@@ -77,7 +75,7 @@ const footerHTML = `
         </div>
         <div class="contact-item">
           <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"/><polyline points="22,6 12,13 2,6"/></svg>
-          <span>pxl220003@gmail.com</span>
+          <span>medsexpresspharmacyrx@gmail.com</span>
         </div>
       </div>
     </div>
@@ -170,7 +168,7 @@ function footerSubmit() {
   const now = new Date().toLocaleString('en-US', { dateStyle: 'full', timeStyle: 'short' });
 
   const templateParams = {
-    to_email:    'pxl220003@gmail.com',
+    to_email:    'medsexpresspharmacyrx@gmail.com',
     from_name:   first + ' ' + last,
     from_email:  email,
     phone:       phone || 'Not provided',
